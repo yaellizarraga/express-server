0ae8532b25bc6ad30a544b9dabcaf3c0138565fa
Yael Lizarraga <yaellizarraga@outlook.es>
2020-10-08 18:03:48 -0600

    feature/adding mysql

--- build/index.js
+++ build/index.js
@@ -4,11 +4,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mysql_1 = __importDefault(require("mysql"));
 const app = express_1.default();
 const port = 4000;
+var IEstado;
+(function (IEstado) {
+    IEstado["PENDIENTE"] = "PENDIENTE";
+    IEstado["CANCELADO"] = "CANCELADO";
+    IEstado["TERMINADO"] = "TERMINADO";
+})(IEstado || (IEstado = {}));
+;
+// Crear conexion a MYSQL
+const configMysql = {
+    host: 'localhost',
+    user: 'root',
+    password: 'palvin2015',
+    database: 'todoApp'
+};
+// Configuracion para conexion MYSQL
+const mysqlConnection = mysql_1.default.createConnection(configMysql);
+// Ruta que utilizara la conexion para hacer querys
 app.get('/hello_world', (req, res) => {
-    res.json({
-        message: 'Hola mundo desde el server'
+    const name = 'Yael';
+    const saludo = {
+        message: `Hola mundo, soy ${name}`
+    };
+    res.json(saludo);
+});
+app.get('/suma', (req, res) => {
+    mysqlConnection.query('SELECT 1 + 1 AS solution', (error, result, fields) => {
+        res.json({
+            error,
+            result,
+            fields
+        });
     });
 });
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,63 @@
 import express from 'express';
+import mysql from 'mysql';
 
 const app: express.Application = express();
 const port = 4000;
 
-type IMessage = {
-  message: string;
+enum IEstado {
+  PENDIENTE='PENDIENTE',
+  CANCELADO='CANCELADO',
+  TERMINADO='TERMINADO'
 };
 
-const message: IMessage = {
-  message: 'Hola mundo'
+type Itodo =  {
+  id: number,
+  titulo: string,
+  descripcion: string,
+  estado: IEstado
 };
 
+type IReponse = {
+  data: {
+    todos: Itodo[]
+  }
+};
+
+// Crear conexion a MYSQL
+const configMysql: mysql.ConnectionConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'palvin2015',
+  database: 'todoApp'
+};
+
+// Configuracion para conexion MYSQL
+const mysqlConnection = mysql.createConnection(configMysql);
+
+// Ruta que utilizara la conexion para hacer querys
 app.get('/hello_world', (req: express.Request, res: express.Response) => {
-  res.json(message);
+
+  const name: string = 'Yael';
+
+  type Saludo = {
+    message: string;
+  };
+  
+  const saludo: Saludo = {
+    message: `Hola mundo, soy ${name}`
+  };
+
+  res.json(saludo);
+});
+
+app.get('/suma', (req: express.Request, res: express.Response) => {
+  mysqlConnection.query('SELECT 1 + 1 AS solution', (error, result, fields) => {
+    res.json({
+      error,
+      result,
+      fields
+    });
+  });
 });
 
 app.listen(port, () => {
